5853d785b333c7ab8166b9bfc311f8fac0915714
Krittin Suphakarn <refreshy1234@gmail.com>
2013-03-19 18:19:11 +0700

    JAMES

--- testformeEIEI.py
+++ testformeEIEI.py
@@ -15,12 +15,21 @@ class Simple_drawing_window(QWidget):
         p.setPen(QColor(0, 0, 0))
         p.setBrush(QColor(0, 127, 0))
         p.drawPolygon([
-            QPoint( 0, 100), QPoint(100, 110),
-            QPoint(130, 100), QPoint(100, 150),
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 200),
+        ])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        #p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+
+        p.drawPolygon([
+            QPoint(50, 200), QPoint(150, 200), QPoint(150, 400),QPoint(50, 400)
         ])
 
         p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
+
 
 def main():
     app = QApplication(sys.argv)
